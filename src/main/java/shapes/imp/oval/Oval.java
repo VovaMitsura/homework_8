5e4308a6cfe4c35967cfab4087a538b93f4e69d3
Vova <vovamitsura@gmail.com>
2022-09-02 22:11:00 +0300

    deleting classes

--- src/main/java/shapes/imp/oval/Oval.java
+++
@@ -1,9 +0,0 @@
-package shapes.imp.oval;
-
-import shapes.sides.one.OneSideShape;
-
-public class Oval extends OneSideShape {
-    protected Oval(double sideA) {
-        super("Oval", sideA);
-    }
-}
